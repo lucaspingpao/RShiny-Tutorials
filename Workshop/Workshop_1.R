b7b419d2f98c7e68ffdfac965aabfdd1d61f6ade
Karina Halevy <karina.halevy@gmail.com>
2020-08-14 03:32:28 -0700

    Workshop 1

---
+++ Workshop/Workshop_1.R
@@ -0,0 +1,34 @@
+# Created by Karina Halevy
+# Last edited: August 14, 2020
+
+# Problem 1: print any (school-appropriate) message to the console that you'd like!
+# Your code here for Problem 1
+
+# Problem 2: create two (school-appropriate) strings and print them in one command by pasting them together.
+# Your code here for Problem 2
+
+# Problem 3: create a vector consisting of the first five positive integers in increasing order.
+  # Then, create a vector consisting of the first five negative integers (-1 through -5) in decreasing order.
+  # Compute and print the component-wise sum of those vectors.
+  # Compute and print the component-wise product of those vectors.
+  # Write the results in a comment below your code.
+  # Concatenate the two vectors and print the result.
+  # Stack the first vector on top of the second vector as a 2-row matrix and print the result.
+  # Combine the vectors as a 2-column matrix (first then second) and print the result.
+  # Retrieve and print the first row of the 2-column matrix you just created.
+  # Retrieve and print the element in the first row, third column of the 2-row matrix.
+  # Retrieve and print the second column of the 2-row matrix.
+  
+# Your code here for Problem 3
+
+# Problem 4: combine your two vectors from Problem 3 into a list.
+  # Retrieve and print the second element of this list.
+  # Name the elements of your list "first element" and "second element"
+  # Retrieve the first element of the list using its name.
+
+# Problem 5: write a loop that prints every odd number from 1 to 20, inclusive.
+
+# Problem 6: write a function that computes the average (mean) of a vector of elements.
+  # Call that function on the example vectors c(1, 2, 3, 4, 5, 6) and c(2, 4, -5, 9).
+
+
