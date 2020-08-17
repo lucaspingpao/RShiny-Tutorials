0c925955263e254016228914c8aab100e2044083
Lucas Pao <lucaspingpao@gmail.com>
2020-08-17 22:27:22 +0200

    New Statistics for Day 3

---
+++ New Day 3 Statistics.R
@@ -0,0 +1,51 @@
+a <- 1:5; b <- 1:5
+multiplicationTable <- outer(a,b, function(a,b) a*b); multiplicationTable
+
+random <- sample(1:10, 5); random
+
+
+# Statistics -----------------------------------------------------------
+#sum, min/max, mean, median, range, IQR, variance, standard deviation
+
+#Applying to all elements in the table
+multiplicationTable
+sum(multiplicationTable)
+min(multiplicationTable)
+max(multiplicationTable)
+mean(multiplicationTable)
+median(multiplicationTable)
+range(multiplicationTable)
+IQR(multiplicationTable)
+sd(multiplicationTable)
+
+#Applying to specific columns
+multiplicationTable
+multiplicationTable[,4]
+min(multiplicationTable[,4])
+max(multiplicationTable[,4])
+mean(multiplicationTable[,4])
+median(multiplicationTable[,4])
+range(multiplicationTable[,4])
+IQR(multiplicationTable[,4])
+var(multiplicationTable[,4])
+sd(multiplicationTable[,4])
+
+random
+min(random)
+max(random)
+mean(random)
+median(random)
+range(random)
+IQR(random)
+var(random)
+sd(random)
+
+
+#Applying sum/mean to all rows/columns
+multiplicationTable
+colSums(multiplicationTable)
+rowSums(multiplicationTable)
+colMeans(multiplicationTable)
+rowMeans(multiplicationTable)
+
+
