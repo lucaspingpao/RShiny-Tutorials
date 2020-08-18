3c37be3a5bb43c26b71f07313d91f545a3c0fbec
Karina Halevy <karina.halevy@gmail.com>
2020-08-18 02:17:16 -0700

    modified day 3

--- New Day 3 Statistics.R
+++ New Day 3 Statistics.R
@@ -2,15 +2,15 @@
 # Created by Lucas Pao
 
 # Creating some random datasets
-a <- 1:5; b <- 1:5
-multiplicationTable <- outer(a,b, function(a,b) a*b); multiplicationTable
+a <- 1:5; b <- 1:5; a; b
+multiplicationTable <- outer(a, b, function(a, b) a * b); multiplicationTable
 random <- sample(1:10, 5); random
 
 
 # Statistics -----------------------------------------------------------
-#sum, min/max, mean, median, range, IQR, variance, standard deviation
+# sum, min/max, mean, median, range, IQR, variance, standard deviation
 
-#Applying to a random set of numbers - let's check the hand-calculations we just did before!
+# Applying to a random set of numbers - let's check the hand-calculations we just did before!
 random
 min(random)
 max(random)
@@ -18,10 +18,10 @@ mean(random)
 median(random)
 range(random)
 IQR(random)
-var(random)
-sd(random)
+var(random) # sample variance
+sd(random) # sample standard deviation
 
-#Applying to all elements in the table
+# Applying to all elements in the table
 multiplicationTable
 sum(multiplicationTable)
 min(multiplicationTable)
@@ -32,17 +32,17 @@ range(multiplicationTable)
 IQR(multiplicationTable)
 sd(multiplicationTable)
 
-#Applying to specific columns
+# Applying to specific columns
 multiplicationTable
-multiplicationTable[,4]
-min(multiplicationTable[,4])
-max(multiplicationTable[,4])
-mean(multiplicationTable[,4])
-median(multiplicationTable[,4])
-range(multiplicationTable[,4])
-IQR(multiplicationTable[,4])
-var(multiplicationTable[,4])
-sd(multiplicationTable[,4])
+col4 <- multiplicationTable[,4]; col4
+min(col4)
+max(col4)
+mean(col4)
+median(col4)
+range(col4)
+IQR(col4)
+var(col4)
+sd(col4)
 
 #Applying sum/mean to all rows/columns
 multiplicationTable
--- Wave 5 RShiny Day 1 Template.R
+++ Wave 5 RShiny Day 1 Template.R
@@ -5,20 +5,32 @@
 # To create a new document, go to File -> New File -> R Script
 # Print hello world!
 # To run a command, move to the line you want to execute and hit Ctrl + Enter or Cmd + Return
-
+print("Hello world!")
 
 # 2. Data types -----------------------------------------------------------
 
 # Assignment operator "<-" assigns the value on the right to the variable on the left
 # Unlike other programming languages, you do not need specify the data type of a variable to create one
 # To show a variable on the R console, simply type the name of that variable
-
+testVariable1 <- 1; testVariable1
+print(testVariable1)
+testVariable2 <- "Hello"; testVariable2
+testVariable3 <- TRUE; testVariable3 # boolean
 
 # You can change the data type of a variable using as.numeric or as.character
-
+testVariable3 <- 2; testVariable3
+testVariable3 <- as.character(testVariable1); testVariable3
+testVariable4 <- as.numeric(testVariable3); testVariable4
 
 # Print hello world again!
+name <- "Karina"
+greeting <- paste("Hello my name is", name)
+print(greeting)
 
+1 + 2
+3 * 8
+3 / 4
+5 - 6
 # no quotes here because we are printing the value inside the variable named greeting, not the word "greeting" itself
 
 # 3. Data structures --------------------------------------------------------------
@@ -26,52 +38,67 @@
 # a) Vectors
 
 # To create a vector, put the elements of a vector inside the c() function
-
+vectorExample <- c(1, 2, 3, 4, 5); vectorExample
 
 # To append or combine vectors, place all the vectors and extra elements into the c() function
-
+combinedVector <- c(vectorExample, 10, 11, 12); combinedVector
 
 # Sum function adds together all the elements of a vector
+sum(vectorExample)
 
-
+v1 <- c(1, 2, 3, 4); v1
+v2 <- c(6, 7, 8, 9); v2
 # Standard operations between two vectors acts on each corresponding element individually to produce a third vector
-
+v1 + v2
+v1 * v2
 
 # b) Matrices
 
 # Concatenates vectors into a matrix
-
+cbind(v1, v2)
+rbind(v1, v2)
 
 # Other way to create matrix
-
+matrixInputs <- c(v1, v2); matrixInputs
+matrixExample <- matrix(matrixInputs, nrow=2, ncol=4, byrow=TRUE); matrixExample
 
 # Accessing elements in a matrix
-
+matrixExample[1,3]
+matrixExample[1,]
+matrixExample[,3]
 
 # Create row/column names and access matrix elements by name
-
+rownames(matrixExample) <- c("row1", "row2"); matrixExample
+colnames(matrixExample) <- c("col1", "col2", "col3", "col4"); matrixExample
+matrixExample["row1", "col3"]
+matrixExample["row1",]
+matrixExample[,"col3"]
 
 # c) Lists
 
 # Create list elements first
-
+line1 <- c(1, 2, 3, 4, 5)
+line2 <- c("A", "B", "C")
 
 # Combine list elements into a list
-
+listExample <- list(line1, line2); listExample
 
 # Access list elements
-
+listExample[[2]]
 
 # Create names for list elements and access them by name
-
+names(listExample) <- c("firstline", "secondline"); listExample
+listExample$secondline
 
 # 4. Conditionals ---------------------------------------------------------
 
 # Synctactic structure of conditional statements
-# if(condition is satisfied) {
-#   run command inside brackets once
-# }
-# 
+ if(2 > 3) {
+   print("two is more than three")
+} else {
+  print("something went wrong")
+}
+
 # while(condition is satisfied) {
 #   keep running brackets inside brackets until conditions are no longer satisfied
 # }
@@ -86,11 +113,20 @@
 1:5 # Vector of elements 1-5
 
 # Iterates through command "print(i)" 5 times
-
+for(i in 1:5) {
+  print(i)
+}
 
 # Iterates through command "print("Hello world") 5 times
+for(i in 1:5) {
+  print("Hello world")
+}
 
-
+n <- 5
+while(n > 0) {
+  print("n is positive")
+  n <- n - 1
+}
 # 5. Functions ------------------------------------------------------------
 
 # Syntactic structure of functions
@@ -101,8 +137,12 @@
 
 
 # Print hello world yet again!
-
-
+greet <- function(name) {
+  message <- paste("Hello my name is", name)
+  return(message)
+}
+greet("Karina")
+greet("Lucas")
 # Input name, output "Hello my name is ... "
 
 
